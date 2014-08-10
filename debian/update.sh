8000f8e89ce6b9c49150775a6a80512b5d55f55e
Mathias Ertl <mati@er.tl>
2014-08-10 19:05:10 +0200

    Backport various changes from the awstats package currently in unstable

--- debian/update.sh
+++ debian/update.sh
@@ -19,15 +19,21 @@ trap 'rm -f $ERRFILE' INT QUIT TERM EXIT
 AWSTATS_NICE=10
 [ ! -r "$DEFAULT" ] || . "$DEFAULT"
 
+# For compatibility: handle empty AWSTATS_ENABLE_CRONTABS as "yes":
+[ "${AWSTATS_ENABLE_CRONTABS:-yes}" = "yes" ] || exit 0
+
 cd /etc/awstats
 
 for c in `/bin/ls -1 awstats.*.conf 2>/dev/null | \
-          /bin/sed 's/^awstats\.\(.*\)\.conf/\1/'` awstats
+          /bin/sed 's/^awstats\.\(.*\)\.conf/\1/'` \
+         `[ -f /etc/awstats/awstats.conf ] && echo awstats`
 do
   if ! nice -n $AWSTATS_NICE $AWSTATS \
 	  -config=$c \
 	  -update >$ERRFILE 2>&1
   then
+    echo "Error while processing" \
+         "/etc/awstats/awstats$(test $c != awstats && echo .$c).conf" >&2
     cat $ERRFILE >&2 # an error occurred
   fi
 done
